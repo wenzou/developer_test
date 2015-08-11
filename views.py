015080d37f04e077c71dd0b9289fa3a4e0d1ade2
wenpeng zou <wen@tailwindweb.com>
2015-08-10 22:36:31 -0700

    updated port config

--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 # We need to import request to access the details of the POST request
 # and render_template, to render our templates (form and response)
 # we'll use url_for to get some URLs for the app on the templates
+import os
 from flask import Flask, render_template, request, url_for
 from objects import CreditManager
 # Initialize the Flask application
@@ -36,9 +37,12 @@ def form():
     else:
         return render_template('form_submit.html')
 
+
 # Run the app :)
+port = int(os.environ.get('PORT', 5000))
+
 if __name__ == '__main__':
   app.run( 
         host="0.0.0.0",
-        port=int("5000")
+        port=port
   )
